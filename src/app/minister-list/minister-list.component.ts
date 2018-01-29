2ea8bcd3399774490ea549a9232034ba3ef0540a
Mahendra Bagul <bagulm123@gmail.com>
2018-01-29 23:33:57 +0530

    Added location support

--- src/app/minister-list/minister-list.component.ts
+++ src/app/minister-list/minister-list.component.ts
@@ -6,6 +6,8 @@ import { SearchService } from './search.service';
 import { GeoService } from '../geo.service';
 import { CurrentLocation } from './currrent-location';
 import { forEach } from '@angular/router/src/utils/collection';
+import { SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
+import { SimpleChange } from '@angular/core/src/change_detection/change_detection_util';
 declare var google: any;
 
 @Component({
@@ -18,10 +20,8 @@ export class MinisterListComponent implements OnInit {
   searchTerm$ = new Subject<string>();
   latitude: number;
   longitude: number;
-  locations: CurrentLocation[];
-  hasValue(obj, key, value) {
-    return obj.hasOwnProperty(key) && obj[key] === value;
-  };
+  private locations: CurrentLocation[];
+  geographicalAreas: any[] = [];
   public getGeoLocation() {
     if (navigator.geolocation) {
       var options = {
@@ -40,7 +40,16 @@ export class MinisterListComponent implements OnInit {
             if (results[0] != null) {
               let city = results[0].address_components[results[0].address_components.length - 4].short_name;
               this.locations = results[0].address_components;
-              console.log(this.locations.some(function (location) { return this.hasValue(location, "long_name", "Pune"); }));
+              for (var location in this.locations) {
+                if (this.locations[location].hasOwnProperty("types")) {
+                  if ((this.locations[location].types.indexOf("administrative_area_level_1") > -1) || (this.locations[location].types.indexOf("administrative_area_level_2") > -1) ||
+                    (this.locations[location].types.indexOf("sublocality_level_1") > -1) ||
+                    (this.locations[location].types.indexOf("neighborhood") > -1) ||
+                    (this.locations[location].types.indexOf("postal_code") > -1)) {
+                    this.geographicalAreas.push(this.locations[location].long_name);
+                  }
+                }
+              }
             } else {
               alert("No address available");
             }
@@ -51,8 +60,9 @@ export class MinisterListComponent implements OnInit {
       }, options);
     }
   }
-
   ngOnInit() {
+    this.getGeoLocation();
+    console.log(this.geographicalAreas);
     this.route.data.map((data) => data['ministers']).subscribe(
       (ministers) => {
         this.ministers = ministers;
@@ -63,14 +73,5 @@ export class MinisterListComponent implements OnInit {
     this.searchService.searchMinisters(this.searchTerm$).subscribe((result) => {
       this.ministers = result;
     });
-    this.getGeoLocation();
-  }
-  private setCurrentPosition() {
-    if (!!navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-      });
-    }
   }
 }
